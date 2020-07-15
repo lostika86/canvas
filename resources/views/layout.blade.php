0dfe1f0ef4b21c9338b79ead844eea66759df578
lostika <eugen.juhos@gmail.com>
2020-07-15 14:58:28 +0200

    fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }} ― Canvas</title>
+    <title>{{ config('app.name') }} ― Canvaska</title>
 
     @if($scripts['darkMode'])
         <link rel="stylesheet" id="baseStylesheet" type="text/css" href="{{ mix('css/app-dark.css', 'vendor/canvas') }}">
